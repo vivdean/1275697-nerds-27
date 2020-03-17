e263076ce22a5bbb549f3b33f912a1c169b7cf31
vivdean <60105628+vivdean@users.noreply.github.com>
2020-03-17 13:00:54 +0300

    правки

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,26 @@
 
     var link = document.querySelector(".write-button");
-  
+
     var popup = document.querySelector(".modal");
     var close = popup.querySelector(".modal-close");
-    
+
     var form = popup.querySelector("form");
     var username = popup.querySelector("[name=username]");
     var email = popup.querySelector("[name=email]");
-            
+
     var isStorageSupport = true;
     var storage = "";
-  
+
     try {
       storage = localStorage.getItem("username");
     } catch (err) {
       isStorageSupport = false;
     }
-    
+
     link.addEventListener("click", function (evt) {
       evt.preventDefault();
       popup.classList.add("modal-show");
-      
+
       if (storage) {
         username.value = storage;
         email.focus();
@@ -28,13 +28,12 @@
         username.focus();
       }
     });
-    
-    close.addEventListener("click", function (evt) {
-      evt.preventDefault();
+
+    close.addEventListener("click", function () {
       popup.classList.remove("modal-show");
       popup.classList.remove("modal-error");
     });
-    
+
     form.addEventListener("submit", function (evt) {
       if (!username.value || !email.value) {
         evt.preventDefault();
@@ -47,7 +46,7 @@
         }
       }
     });
-    
+
     window.addEventListener("keydown", function (evt) {
       if (evt.keyCode === 27) {
         if (popup.classList.contains("modal-show")) {
